6dc116a2156196ba7d1f58f2a1f256a21da4343f
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2014-01-27 20:03:44 +0100

    Swapped comment box and messages stream

--- src/mouthpiece/routes/home.clj
+++ src/mouthpiece/routes/home.clj
@@ -47,15 +47,17 @@
                    [:h1 "Mouthpiece"]]
                   [:div {:class "small-12 columns"}
                    [:p "Welcome, say whatever you want."]]]
+
                  [:hr]
+
                  [:div {:class "row"}
-                  [:div {:class "small-12 large-6 columns"}
-                   (show-messages)]
                   [:div {:class "small-12 large-6 columns"}
                    (when error
                      [:div {:class "alert-box warning round"}
                       error])
-                   (comment-box message)]]
+                   (comment-box message)]
+                  [:div {:class "small-12 large-6 columns"}
+                   (show-messages)]]
 
                  [:div {:class "row"}
                   [:hr]
