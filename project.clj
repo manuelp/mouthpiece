7445cc1aeead34d84f33a6af8e2e6f0416ad226e
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2014-01-28 21:40:56 +0100

    Correct description and URL in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject mouthpiece "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Simple anonymous guestbook, to spread ideas."
+  :url "https://github.com/manuelp/mouthpiece"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
