01220a757431ec99253fdd791f67dd70aa9c3659
Manuel Paccagnella <manuel.paccagnella@visionest.com>
2014-04-03 10:40:09 +0200

    Upgrade to Clojure 1.6.0 and (ring-devel 1.2.2, lein-light-nrepl 0.0.17)

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "https://github.com/manuelp/mouthpiece"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.5"]
                  [ring-server "0.3.1"]
                  [org.clojure/java.jdbc "0.2.3"]
                  [org.xerial/sqlite-jdbc "3.7.2"]
                  [markdown-clj "0.9.41"]
-                 [lein-light-nrepl "0.0.15"]
+                 [lein-light-nrepl "0.0.17"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
   :plugins [[lein-ring "0.8.7"]]
   :ring {:handler mouthpiece.handler/app
@@ -21,5 +21,5 @@
                                  :stacktraces? false
                                  :auto-reload? false}}
              :dev {:dependencies [[ring-mock "0.1.5"]
-                                  [ring/ring-devel "1.2.1"]]
+                                  [ring/ring-devel "1.2.2"]]
                    :ring {:open-browser? false}}})
